cb0601d93853c0c87932685ca34e946e2fc858f3
eugenekhoo <ktw1995@gmail.com>
2023-08-22 17:01:25 +0800

    updated readme, routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Jobs from "./pages/Jobs";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/1" />} />
           <Route path="/:page?" element={<Jobs />} />
         </Routes>
       </BrowserRouter>
--- src/README.md
+++ src/README.md
@@ -1,1 +1,3 @@
 # Nodeflair Software Engineering Internship Assignment
+
+[Link to project](https://nodeflair-assignment-gamma.vercel.app/)
